89f80e098339730f49e4e3c8fc8bf6f9ee794252
Alvindez <dezwasswa4@gmail.com>
2023-09-11 16:27:14 +0300

    change

--- today.py
+++ today.py
@@ -1,2 +1,8 @@
 def dez():
-    name="alvin" 
+    name="alvin"
+    age= 24
+    course= "Python programming"
+    salary= 400"dollars monthly"
+    marital_status_married= False
+    print I am (name) aged (age) doing (course), I earn around (salary) being married (marital_status_married)
+dez()
