a696dceea977f9afdbd22c2c235ba8ee36779ffa
Alvindez <dezwasswa4@gmail.com>
2023-09-11 17:15:49 +0300

    changes

--- data_structures.py
+++ data_structures.py
@@ -1,5 +1,5 @@
 def input_func():
-    my_name= input("Your Name")
+    my_name= input("Your name")
     age=int(input("Your age"))
     married= input("Are you single")
     print(f"My name is {my_name} I am {age} and me being married is {married}")
