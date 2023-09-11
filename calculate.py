326c91e09248f43bc41782388d442bb42b5c3de0
Alvindez <dezwasswa4@gmail.com>
2023-09-11 18:03:49 +0300

    changes

---
+++ calculate.py
@@ -0,0 +1,10 @@
+def calc(a, b, c):
+    output= a + b - c
+    return output
+
+b1=int(input("first number"))
+b2= int(input("second number"))
+b3= int(input("third number"))
+
+
+print(calc(b1,b2,b3))
