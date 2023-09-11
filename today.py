65d5425ab2be663bcf81ef6c44627384a33abcbd
Alvindez <dezwasswa4@gmail.com>
2023-09-11 16:34:12 +0300

    change

--- today.py
+++ today.py
@@ -4,5 +4,5 @@ def dez():
     course= "Python programming"
     salary= 400"dollars monthly"
     marital_status_married= False
-    print I am (name) aged (age) doing (course), I earn around (salary) being married (marital_status_married)
+    print(f"I am {name} aged {age} doing {course}, I earn around {salary} being married {marital_status_married}")
 dez()
